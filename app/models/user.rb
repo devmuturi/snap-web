8160fc6b6cfdb51a27fb2178cfe314ada4c0bb1b
devmuturi <muturinjonge09@gmail.com>
2025-10-04 19:37:20 +0300

    validate the models and associate them

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  before_validation :remove_extra_whitespace
+
+  private
+
+  def remove_extra_whitespace
+    self.name = self.name&.strip
+  end
 end
