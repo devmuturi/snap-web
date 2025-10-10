940ccec1cc64ef7cf89547e3d672be858c9a4317
devmuturi <muturinjonge09@gmail.com>
2025-10-10 12:44:32 +0300

    State machine logic for the file field

---
+++ app/javascript/controllers/image_upload_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="image-upload"
+export default class extends Controller {
+  static = [
+    "preview",
+    "cta",
+    "progress",
+    "remove"
+  ]
+
+  connect() {
+    if (this.previewTarget.getAttribute("src")) {
+      this.setState("image_set")
+    } else {
+      this.setState("no_image")
+    }
+  }
+
+  // private
+
+  setState(state) {
+    switch(state) {
+      case "no image":
+        this.ctaTarget.classList.remove("is-hidden")
+        this.removeTarget.classList.add("is-hidden")
+        this.progressTarget.classList.add("is-hidden")
+        break
+      case "uploading":
+        this.ctaTarget.classList.remove("is-hidden")
+        this.removeTarget.classList.add("is-hidden")
+        this.progressTarget.classList.remove("is-hidden")
+        break
+      case "image_set":
+        this.ctaTarget.classList.add("is-hidden")
+        this.removeTarget.classList.remove("is-hidden")
+        this.progressTarget.classList.add("is-hidden")
+        break
+    }
+  }
+}
