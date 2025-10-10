b392f017e62ce5a1b08f7c500732145595a000b1
devmuturi <muturinjonge09@gmail.com>
2025-10-10 11:25:07 +0300

    set libvips for active_storage

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Snap
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # set active_storage to use libvips
+    config.active_storage.variant_processor = :vips
   end
 end
