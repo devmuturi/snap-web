07c252d50b88df122e0955b1cbc05f3e0c6ff1f0
devmuturi <muturinjonge09@gmail.com>
2025-10-12 16:35:02 +0300

    Saving and updating draft ads

--- app/controllers/listings/drafts_controller.rb
+++ app/controllers/listings/drafts_controller.rb
@@ -1,7 +1,45 @@
 class Listings::DraftsController < ApplicationController
+    before_action :load_listing, only: :update
+
     def create
+        @listing = Listing.new(
+            listing_params.with_defaults(
+                creator: current_user
+                organization: current_user.organization,
+                status: :draft
+            )
+        )
+
+        if @listing.save
+            redirect_to listing_path(@listing), flash: { success: t(".success") }, status: :see_other
+        else
+            render "listings/new", status: :unprocessable_entity
+        end
     end
 
     def update
-    end
+        @listing.assign_attributes(
+            listing_params.with_defaults(
+                status: :draft
+            )
+        )
+
+        if @listing.save
+            redirect_to listing_path(@listing), status: :see_other, flash: { success: t(".success") }
+        else
+            render "listings/edit", status: :unprocessable_entity
+        end
+    
+        
+        private
+
+        def listing_params
+            params.require(:listing).permit(
+                Listing.permitted_attributes
+            )
+        end
+
+        def load_listing
+            @listing = Listing.find(params[:listing_id])
+        end
 end
