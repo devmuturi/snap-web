6a20009a45a463c5ea771c8583eed8800a59109b
devmuturi <muturinjonge09@gmail.com>
2025-10-12 16:04:47 +0300

    Add strings for listings breadcrumbs

--- app/controllers/my_listings_controller.rb
+++ app/controllers/my_listings_controller.rb
@@ -1,6 +1,8 @@
 class MyListingsController < ApplicationController
     before_action :authenticate_user!
 
+    drop_breadcrumb I18n.t("my_listings.show.title")
+
     def show
         organization = current_user.organizations.first
         @pagy, @listings = pagy(organization.listings)
