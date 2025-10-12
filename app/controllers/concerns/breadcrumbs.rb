d36838a2fe5dff876c01763b5d79c1c0365862aa
devmuturi <muturinjonge09@gmail.com>
2025-10-12 15:48:42 +0300

    Drop a breadcrumb at class level

--- app/controllers/concerns/breadcrumbs.rb
+++ app/controllers/concerns/breadcrumbs.rb
@@ -1,3 +1,28 @@
 module Breadcrumbs
     extend ActiveSupport::Concern
+
+    included do
+        helper_method :breadcrumbs
+    end
+
+    class_methods do
+        def drop_breadcrumb(*args, **options)
+            label, path = args
+
+            before_action(options) do |controller|
+                controller.drop_breadcrumb(label, path)
+            end
+        end
+    end
+
+    protected
+
+    def drop_breadcrumb(label, path = nil)
+    end
+
+    private
+
+    def breadcrumbs
+        @breadcrumbs ||= Trail.new
+    end
 end
