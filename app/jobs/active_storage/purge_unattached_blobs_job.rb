b4c675c12c5525b3277e7818630608fa142b8f36
devmuturi <muturinjonge09@gmail.com>
2025-10-17 02:27:50 +0300

    Fix the cron job errors

--- app/jobs/active_storage/purge_unattached_blobs_job.rb
+++ app/jobs/active_storage/purge_unattached_blobs_job.rb
@@ -2,7 +2,7 @@ class ActiveStorage::PurgeUnattachedBlobsJob < ApplicationJob
   queue_as :default
 
   def perform(listing)
-    ActiveStorage::Blob.PurgeUnattachedBlobsJob
+    ActiveStorage::Blob.unattached
     .where("active_storage_blobs.created_at <= ?", 2.days.ago)
     .find_each(&:purge_later)
   end
